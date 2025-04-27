dda8743ef2044c0b42c23bf5fb90386cc4b29cb0
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-27 12:35:45 +0300

    linked

---
+++ src/components/form.js
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import step1 from './Step1';
+import step2 from './Step2';
+import step3 from './Step3';
+import step4 from './Step4';
+import step5 from './Step5';
+import "./form.css";
+
